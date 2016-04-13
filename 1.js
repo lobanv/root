6ece87c7ab34613c2684ea2bb04126a7412e113e
lobanv <viktarloban@outlook.com>
2016-04-13 15:19:36 +0300

    data added

--- 1.js
+++ 1.js
@@ -1,7 +1,6 @@
-var src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/%D0%A1%D1%80%D0%B0%D0%B2%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5_%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%80%D1%8B_%D0%BA%D0%BE%D0%BC%D0%BF%D0%BE%D0%BD%D0%B5%D0%BD%D1%82%D0%BE%D0%B2_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B_%CE%B1_%D0%A6%D0%B5%D0%BD%D1%82%D0%B0%D0%B2%D1%80%D0%B0_%D0%B8_%D0%A1%D0%BE%D0%BB%D0%BD%D1%86%D0%B0.jpg/1920px-%D0%A1%D1%80%D0%B0%D0%B2%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5_%D1%80%D0%B0%D0%B7%D0%BC%D0%B5%D1%80%D1%8B_%D0%BA%D0%BE%D0%BC%D0%BF%D0%BE%D0%BD%D0%B5%D0%BD%D1%82%D0%BE%D0%B2_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B_%CE%B1_%D0%A6%D0%B5%D0%BD%D1%82%D0%B0%D0%B2%D1%80%D0%B0_%D0%B8_%D0%A1%D0%BE%D0%BB%D0%BD%D1%86%D0%B0.jpg';
 function AddImage()
 {
-document.getElementById('Container').innerHTML = '<img src="' + src + '" alt="test image" width="300" height="200" />';
+document.getElementById('Container').innerHTML = '<img src="' + imgUrl1 + '" alt="test image" width="300" height="200" />';
 }
 function AddImage2(target, src)
 {
@@ -9,8 +8,7 @@ target.src = src;
 }
 function AddImage3(target, src)
 {
-target.src = src + '.png';
-}
-function TestLink()
-{
+var img = document.createElement('img');
+img.src = src;
+target.appendChild(img);
 }
---
+++ data.js
@@ -0,0 +1,4 @@
+var imgUrl1 = 'https://i.ytimg.com/vi/AzemixD0_Kg/maxresdefault.jpg';
+var imgUrl2 = 'http://news.povray.org/povray.binaries.images/attachment/%3C4bc4f9f4@news.povray.org%3E/Saturn.png';
+var imgUrl3 = 'http://i64.tinypic.com/2jbqrsh';
+var imgUrl4 = 'http://www.binarysurge.com/images/2010.08.05-1987A.jpg';
